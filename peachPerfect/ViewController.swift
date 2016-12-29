25ca7b7fe908c60ea94933910983d303fc8fcae9
haung921209 <haung921209@gmail.com>
2016-12-30 00:50:12 +0900

    Record & Stop 버튼 추가 및 label 추가

--- peachPerfect/ViewController.swift
+++ peachPerfect/ViewController.swift
@@ -20,6 +20,13 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func recordButton(_ sender: Any) {
+        
+        
+        
+        
+        
+    }
 
 
 }
