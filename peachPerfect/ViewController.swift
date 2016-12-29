6ea5165531bed6b48f04e0a6b0aa4511fe225819
haung921209 <haung921209@gmail.com>
2016-12-30 04:54:15 +0900

    Recording Code 추가

--- peachPerfect/ViewController.swift
+++ peachPerfect/ViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
 
+    var audioRecorder:AVAudioRecorder!
     override func viewDidLoad() {
         
         super.viewDidLoad()
@@ -24,10 +25,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
     @IBAction func recordButton(_ sender: Any) {
-        recordingLabel.text="Recording in Progress"
-        stopRecordingButton.isEnabled=true
-        recordButton.isEnabled=false
+        recordingLabel.text = "Recording in progress"
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
+        
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, with:AVAudioSessionCategoryOptions.defaultToSpeaker)
+        
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
         
     }
     @IBOutlet weak var recordingLabel: UILabel!
